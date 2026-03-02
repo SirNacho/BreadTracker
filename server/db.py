01e8b218f677e49ce9f43c604266c64913e57068
SirNacho <steven.frausto@nachoweb.tech>
2026-03-01 23:47:15 -0600

    Adjusted both BackendNotes.md and db.py to better work with the current api key in the repo. I also make loadSupabaseConnection(url, key) to make it easier to create connections without running db.py by importing db.py to other files.

--- server/db.py
+++ server/db.py
@@ -6,14 +6,29 @@ from supabase import create_client, Client
 #Supabase Configurations Section
 load_dotenv()
 
-url: str = os.environ.get("SUPABASE_URL")
-key: str = os.environ.get("SUPABASE_KEY")
+url: str = os.environ.get("SUPABASE_PROJECT_URL")
+key: str = os.environ.get("SUPABASE_API_KEY")
 
 if not url or not key:
     raise ValueError("The supabase url and key is not found. Something is wrong with the dotenv.")
 
 supabase: Client = create_client(url, key)
+print("db.py has successfully created a supabase client connection!")
 
+'''
+This function, "loadSupabaseConnection(url, key)" is for in case front end needed a way to connect
+the supabase connection without running db.py.
+'''
+def loadSupabaseConnection(url : str, key : str):
+    if not url or not key:
+        raise ValueError("The supabase url and key is not found. Something is wrong with either the inputted parameters.")
+
+    try:
+        supabase: Client = create_client(url, key)
+        print("Supabase client has successfully been created!")
+    except Exception as e:
+        print(f"The supabase client is unable to be created. Check what's up with the url and api key.")
+        return None
 
 
 #User Features Section
@@ -90,14 +105,23 @@ def logout_user():
         return None
 
 def main():
+
+    #Example usage:
     #create_user("test@example.com", "test123", "John", "Smith")
     #login_user("test@example.com", "test123")
     #change_user_password("test123")
     #logout_user()
 
+    print("This db.py code has run! Please feel free to look at main() to see how to use the functions in db.py!")
+
 if __name__ == "__main__":
     main()
-    
+
+'''
+Not sure what this code below is for. I'll leave this here just in case. -SF
+'''
+
+'''
 from sqlmodel import create_engine, Session, SQLModel
 
 load_dotenv()
@@ -109,3 +133,4 @@ engine = create_engine(DATABASE_URL, echo=True)
 def get_session():
     with Session(engine) as session:
         yield session
+'''
